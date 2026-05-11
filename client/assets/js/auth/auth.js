03f0b3d82f4d2f0d3e8d8546c0cb537be274a851
SubhadeepM6102002 <subhadeepmanna6102002@gmail.com>
2026-05-11 07:59:58 +0530

    Connected frontend to production backend

--- client/assets/js/auth/auth.js
+++ client/assets/js/auth/auth.js
@@ -10,7 +10,7 @@ async function login(event) {
   }
 
   try {
-    const response = await fetch("https://ai-educator.onrender.com", {
+    const response = await fetch("https://ai-educator.onrender.com/login", {
       method: "POST",
 
       headers: {
--- client/assets/js/auth/signup.js
+++ client/assets/js/auth/signup.js
@@ -10,7 +10,7 @@ async function signup(event) {
   }
 
   try {
-    const response = await fetch("http://localhost:5000/signup", {
+    const response = await fetch("https://ai-educator.onrender.com/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
